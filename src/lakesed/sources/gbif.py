94d5069efb1b58f20505e6612a8d9c10e7def6bc
Amanda Grimm <agrimmps@gmail.com>
2026-04-23 15:44:56 -0400

    add pseudoabsence processing

--- src/lakesed/sources/gbif.py
+++ src/lakesed/sources/gbif.py
@@ -162,3 +162,77 @@ def run_gbif(my_vars, startyear, outfolder, today, userdata, skip=[]):
         print("finished with ", k)
 
     return gbif_obs, gbif_observers
+
+
+def run_gbif_pseudoabsences(my_vars, gbif_observers, startyear, userdata, skip=[]):
+
+    from pygbif import species as species
+    from pygbif import occurrences as occ
+    from pygbif.occurrences.download import GbifDownload
+
+    import os, math, zipfile
+    from time import sleep
+    import pandas as pd
+
+    pabs = {}
+
+    for i, (k, v) in enumerate(my_vars.items()):
+        if k in skip:
+            continue
+
+        print("downloading ", k)
+
+        observers = gbif_observers[k]
+        observers = [x for x in observers if x == x]
+        observers.sort(key=str.lower)
+
+        start = 0
+        end = len(observers)
+        step = 50
+
+        for x in range(start, end, step):
+            observers_chunk = observers[x:x+step]
+
+            print(f'chunk {x}: ', observers_chunk[0])
+
+            gbif_query = GbifDownload(
+                userdata.get('GBIF_USER'),
+                userdata.get('GBIF_EMAIL')
+            )
+
+            gbif_query.add_predicate_dict({"type": "equals", "key": 'HAS_COORDINATE', 'value': 'TRUE', "matchCase": "false"})
+            gbif_query.add_predicate_dict({"type": "equals", "key": 'HAS_GEOSPATIAL_ISSUE', 'value': 'FALSE', "matchCase": "false"})
+            gbif_query.add_predicate_dict({"type": "within", "geometry": "POLYGON((-100.551 36.917,-71.79 36.917,-71.79 49.612,-100.551 49.612,-100.551 36.917))"})
+            gbif_query.add_predicate_dict({"type": "greaterThanOrEquals", "key": 'YEAR', 'value': startyear, "matchCase": "false"})
+            gbif_query.add_predicate_dict({"type": "in", "key": 'RECORDED_BY', 'values': observers_chunk, "matchCase": "false"})
+
+            # taxon filters (unchanged)
+            if k == 'fish':
+                gbif_query.add_predicate_dict({"type": "equals", "key": 'TAXON_KEY', 'value': 44, "matchCase": "false"})
+            if k == 'plant':
+                gbif_query.add_predicate_dict({"type": "equals", "key": 'TAXON_KEY', 'value': 6, "matchCase": "false"})
+            if k == 'invert':
+                gbif_query.add_predicate_dict({"type": "equals", "key": 'TAXON_KEY', 'value': 1, "matchCase": "false"})
+                gbif_query.add_predicate_dict({"type": "not", "predicate": {"type": "equals", "key": 'TAXON_KEY', 'value': 44}})
+
+            xx = gbif_query.post_download(
+                userdata.get('GBIF_USER'),
+                userdata.get('GBIF_PWD')
+            )
+
+            while True:
+                print(f"waiting to get download {xx}...")
+                status = occ.download_meta(key=xx)['status']
+
+                if status not in ['PREPARING', 'RUNNING']:
+                    if status == 'SUCCEEDED':
+                        output_path = k+"_gbif_observer_potential_pseudoabs"
+                        if not os.path.exists(output_path):
+                            os.mkdir(output_path)
+
+                        occ.download_get(xx, output_path)
+                    break
+
+                sleep(20)
+
+    return pabs
